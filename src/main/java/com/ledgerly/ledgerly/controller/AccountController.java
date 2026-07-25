b49dfa8b70a78bd34a41f75e50d00ffdd2ef1b56
Neha mangal <nehamangal@Nehas-MacBook-Air.local>
2026-07-25 19:11:08 +0530

    Update pom.xml and AccountController for Phase 1 backend

--- src/main/java/com/ledgerly/ledgerly/controller/AccountController.java
+++ src/main/java/com/ledgerly/ledgerly/controller/AccountController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
-    
     @Autowired
     private AccountService accountService;
 
